fde289cf18190f43d4be1dd97a85c35e6ff45a5d
pad19 <pad19051999@gmail.com>
2019-12-27 11:24:22 +0200

    refactor

--- js/app.js
+++ js/app.js
@@ -2,12 +2,40 @@
 
 /**------------------------------------------------------------*/
 
-const inn = '7705432475'.split('').map(el => +el);
+// const inn = '7705432475'.split('').map(el => +el);
+const inn = '500100732259'.split('').map(el => +el);
 
 /**-------------------------------------------------------------*/
 
-const factors = [2,4,10,3,5,9,4,6,8,0];
+const factors10 = [2,4,10,3,5,9,4,6,8,0];
+const factors11 = [7,2,4,10,3,5,9,4,6,8,0];
+const factors12 = [3,7,2,4,10,3,5,9,4,6,8,0];
 let sum = 0;
-inn.forEach((el, index) => sum += el * factors[index]);
-let res = sum - Math.floor(sum / 11) * 11;
-res === inn[inn.length - 1] ? console.log(`${res} = ${inn[inn.length - 1]} inn is correct`) : console.log(`${res} != ${inn[inn.length - 1]} inn is incorrect`);
+let sum2 = 0;
+let res, res2;
+const lastNum = inn[inn.length - 1];
+const penultimateNum = inn[inn.length - 2];
+
+switch (inn.length) {
+    case 10:
+        inn.forEach((el, index) => sum += el * factors10[index]);
+        res = sum - Math.floor(sum / 11) * 11;
+        res > 9 ?
+            res / 10 === lastNum ? console.log(`${res / 10} = ${lastNum} inn is correct`) : console.log(`${res} != ${lastNum} inn is incorrect`) :
+            res === lastNum ? console.log(`${res} = ${lastNum} inn is correct`) : console.log(`${res} != ${lastNum} inn is incorrect`);
+        break;
+    case 12:
+        inn.forEach((el, index) => index !== inn.length - 1 ? sum2 += el * factors11[index] : null);
+        res2 = sum2 - Math.floor(sum2 / 11) * 11;
+        res2 > 9 ? res2 /= 10 : null;
+        inn.forEach((el, index) => sum += el * factors12[index]);
+        res = sum - Math.floor(sum / 11) * 11;
+        res > 9 ? res /= 10 : null;
+        res === lastNum && res2 === penultimateNum ?
+            console.log(`${res} = ${lastNum} and ${res2} = ${penultimateNum} inn is correct`) :
+            console.log(`${res} = ${lastNum} and ${res2} = ${penultimateNum} inn is incorrect`);
+        break;
+    default:
+        console.log('default');
+        break;
+}
